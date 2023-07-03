0cb346be72097452c6e38dd579efa38ccfda13d0
Daniel Janus <dj@danieljanus.pl>
2023-07-03 17:47:13 +0200

    Define a tree struct
    I don't yet understand references or borrowing, so the type definition
    may (and probably will) change, but it's a start.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+pub struct Tree<T> {
+    value: T,
+    children: Vec<Tree<T>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t = Tree {
+        value: String::from("root item"),
+        children: vec![Tree {value: String::from("child"), children: vec![]},
+                       Tree {value: String::from("another child"), children: vec![]}]
+    };
+
+    println!("Hello, world! Tree root is: {0} and first child is {1}", t.value, t.children[0].value);
 }
