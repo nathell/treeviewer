cfe2d501ccf68127c7e4493f9ddb1d078a2aa7a2
Daniel Janus <dj@danieljanus.pl>
2023-07-05 17:05:40 +0200

    Smarter implementation of print_tree
    This can actually print the whole tree, with appropriately indented
    nodes (although without any semigraphical visual cues as to the tree
    structure).
    
    Also, replace the hardcoded tree with a more complex one.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
+use std::fmt::Display;
+
 pub struct Tree<T> {
     value: T,
     children: Vec<Tree<T>>,
 }
 
-fn print_tree<T: std::fmt::Display>(t: &Tree<T>) {
-    println!("Hello, world! Tree root is: {0} and first child is {1}", t.value, t.children[0].value);
+fn print_tree_with_prefix<T: Display>(prefix: &str, t: &Tree<T>) {
+    println!("{0}{1}", prefix, t.value);
+    let subprefix = format!("  {}", prefix);
+    for child in t.children.iter() {
+        print_tree_with_prefix(&subprefix, child);
+    }
+}
+
+fn print_tree<T: Display>(t: &Tree<T>) {
+    print_tree_with_prefix("", t);
 }
 
 fn main() {
     let t = Tree {
-        value: String::from("root item"),
-        children: vec![Tree {value: String::from("child"), children: vec![]},
-                       Tree {value: String::from("another child"), children: vec![]}]
+        value: "raz",
+        children: vec![
+            Tree {value: "dwa", children: vec![
+                Tree {value: "trzy", children: vec![]},
+                Tree {value: "cztery", children: vec![]},
+            ]},
+            Tree {value: "pięć", children: vec![
+                Tree {value: "sześć", children: vec![
+                    Tree {value: "siedem", children: vec![]},
+                ]},
+            ]},
+            Tree {value: "osiem", children: vec![]}]
     };
 
     print_tree(&t);
