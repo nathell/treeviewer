ba8c344f08a60f3aab0f7ca951a3f2fa8b44b6b3
Daniel Janus <dj@danieljanus.pl>
2023-07-05 18:32:08 +0200

    Even smarter print_tree
    This uses Unicode box-drawing characters to provide visual cues.

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,22 @@ pub struct Tree<T> {
     children: Vec<Tree<T>>,
 }
 
-fn print_tree_with_prefix<T: Display>(prefix: &str, t: &Tree<T>) {
-    println!("{0}{1}", prefix, t.value);
-    let subprefix = format!("  {}", prefix);
-    for child in t.children.iter() {
-        print_tree_with_prefix(&subprefix, child);
+fn print_tree_with_prefix<T: Display>(prefix: &str, prefix1: &str, prefix2: &str, t: &Tree<T>) {
+    println!("{0}{1}{2}", prefix, prefix1, t.value);
+    let mut it = t.children.iter().peekable();
+
+    let subprefix = format!("{0}{1}", prefix, prefix2);
+
+    while let Some(child) = it.next() {
+        match it.peek() {
+            None => print_tree_with_prefix(&subprefix, "└─ ", "   ", child),
+            Some(_) => print_tree_with_prefix(&subprefix, "├─ ", "│  ", child),
+        }
     }
 }
 
 fn print_tree<T: Display>(t: &Tree<T>) {
-    print_tree_with_prefix("", t);
+    print_tree_with_prefix("", "", "", t);
 }
 
 fn main() {
