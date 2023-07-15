ea3560e97c7e385184d5bf5332e070a8c7490cea
Daniel Janus <dj@danieljanus.pl>
2023-07-15 16:01:00 +0200

    record.sh: avoid losing the most recent commit with clean tree
    When `git commit` fails to create a commit because nothing was added
    to the index with git add -A, we don't want the reset step.

--- scripts/record.sh
+++ scripts/record.sh
@@ -8,9 +8,7 @@ COMMIT_DESC="Build attempt `date`"
 git stash push -k -u
 git stash apply
 git add -A
-git commit -m "$COMMIT_DESC"
-git tag -a -m "$COMMIT_DESC" "$TAG"
-git reset --hard HEAD^
+git commit -m "$COMMIT_DESC" && git tag -a -m "$COMMIT_DESC" "$TAG" && git reset --hard HEAD^
 git stash pop
 
 } >/dev/null
