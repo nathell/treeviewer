4b502a9bfd4143daf45cfa1272b5a6a447c1b449
Daniel Janus <dj@danieljanus.pl>
2023-07-16 13:49:56 +0200

    Rewrite print_tree to use an iterator
    Adds a `Tree.lines()` method that returns an iterator returning
    strings that make up a formatted representation of the tree. With
    this, the implementation of `print_tree` becomes trivial. But it's
    now also more reusable, as we'll now be able to use the iterator to
    render a Cursive view of the tree.
    
    This implementation is based on the "bad" (TBH not so bad...) approach
    from Amos Wenger's article "Recursive Iterators in Rust":
    https://fasterthanli.me/articles/recursive-iterators-rust

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,69 @@
 use std::fmt::Display;
 use std::io::{self, BufRead};
+use std::iter::{empty, Peekable};
+use std::slice::Iter;
 
 pub struct Tree<T> {
     value: T,
     children: Vec<Tree<T>>,
 }
 
-fn print_tree_with_prefix<T: Display>(prefix: &str, prefix1: &str, prefix2: &str, t: &Tree<T>) {
-    if !prefix1.is_empty() {
-        println!("{0}{1}{2}", prefix, prefix1, t.value);
-    }
+pub struct TreeIterator<'a, T> {
+    prefix: String,
+    prefix1: &'static str,
+    prefix2: &'static str,
+    value: &'a T,
+    emitted: bool,
+    viter: Box<dyn Iterator<Item = String> + 'a>,
+    citer: Box<Peekable<Iter<'a, Tree<T>>>>,
+}
 
-    let mut it = t.children.iter().peekable();
-    let subprefix = format!("{0}{1}", prefix, prefix2);
+impl<'a, T> Iterator for TreeIterator<'a, T> where T: Display {
+    type Item = String;
 
-    while let Some(child) = it.next() {
-        match it.peek() {
-            None => print_tree_with_prefix(&subprefix, "└─ ", "   ", child),
-            Some(_) => print_tree_with_prefix(&subprefix, "├─ ", "│  ", child),
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.emitted && !self.prefix1.is_empty() {
+            self.emitted = true;
+            Some(format!("{0}{1}{2}", self.prefix, self.prefix1, self.value))
+        } else if let Some(val) = self.viter.next() {
+            Some(val)
+        } else {
+            if let Some(child) = self.citer.next() {
+                let subprefix = format!("{0}{1}", self.prefix, self.prefix2);
+                let last = !self.citer.peek().is_some();
+                let prefix1 = if last { "└─ " } else { "├─ " };
+                let prefix2 = if last { "   " } else { "│  " };
+                self.viter = Box::new(child.prefixed_lines(subprefix, prefix1, prefix2));
+                self.next()
+            } else {
+                None
+            }
         }
     }
 }
 
+impl<T> Tree<T> where T: Display {
+    pub fn prefixed_lines<'a>(&'a self, prefix: String, prefix1: &'static str, prefix2: &'static str) -> TreeIterator<'a, T> {
+        TreeIterator {
+            prefix: prefix,
+            prefix1: prefix1,
+            prefix2: prefix2,
+            value: &self.value,
+            emitted: false,
+            viter: Box::new(empty()),
+            citer: Box::new(self.children.iter().peekable()),
+        }
+    }
+
+    pub fn lines<'a>(&'a self) -> TreeIterator<'a, T> {
+        self.prefixed_lines(String::from(""), "", "")
+    }
+}
+
 fn print_tree<T: Display>(t: &Tree<T>) {
-    print_tree_with_prefix("", "", "", t);
+    for line in t.lines() {
+        println!("{}", line);
+    }
 }
 
 fn append_path<'a>(mut t: &mut Tree<&'a str>, path: &'a str) {
