4bce7c0736d24cd0a0133b92d32ff718600dc57f
Daniel Janus <dj@danieljanus.pl>
2024-06-09 15:30:08 +0200

    (Un)collapse currently highlighted node
    We add a method Tree::coords to convert a usize (currently
    selected in the SelectView) to the node coordinates (Vec<i32>)
    as per `collapsed`, and use it in the collapse/uncollapse
    callback. Also, rewrite redraw() and both callbacks to use
    call_on_name("select") to avoid double borrowing of the
    SelectView.

--- src/main.rs
+++ src/main.rs
@@ -62,9 +62,7 @@ fn init_state() -> State {
         append_path(&mut t, &line.unwrap());
     }
 
-    let hs = HashSet::from([vec![0, 1, 1]]);
-
-    State { tree: t, collapsed: hs /* HashSet::new() */ }
+    State { tree: t, collapsed: HashSet::new() }
 }
 
 impl<'a, T> Iterator for TreeIterator<'a, T> where T: Display {
@@ -100,6 +98,22 @@ impl<'a, T> Iterator for TreeIterator<'a, T> where T: Display {
     }
 }
 
+impl<T> Tree<T> {
+    pub fn count(&self, coords: &mut Vec<i32>, collapsed: &HashSet<Vec<i32>>) -> usize {
+        if collapsed.contains(coords) {
+            return 1;
+        }
+        let mut res = 1;
+        coords.push(0);
+        for child in &self.children {
+            res += child.count(coords, collapsed);
+            *coords.last_mut().unwrap() += 1;
+        }
+        coords.pop();
+        res
+    }
+}
+
 impl<T> Tree<T> where T: Display {
     pub fn prefixed_lines<'a>(&'a self, parent_prefix: String, immediate_prefix: &'static str, parent_suffix: &'static str, collapsed: &'a HashSet<Vec<i32>>, current: Vec<i32>) -> TreeIterator<'a, T> {
         TreeIterator {
@@ -120,14 +134,50 @@ impl State {
     pub fn lines<'a>(&'a self) -> TreeIterator<'a, String> {
         self.tree.prefixed_lines(String::from(""), "", "", &self.collapsed, vec![0])
     }
+
+    fn recursive_coords(&self, tree: &Tree<String>, mut i: usize, mut coords: Vec<i32>) -> Vec<i32> {
+        if i == 0 {
+            coords
+        } else {
+            i -= 1;
+            let mut next_child: Option<&Tree<String>> = None;
+            coords.push(0);
+            for child in &tree.children {
+                next_child = Some(child);
+                let cnt = child.count(&mut coords, &self.collapsed);
+                if i >= cnt {
+                    i -= cnt;
+                    *coords.last_mut().unwrap() += 1;
+                } else {
+                    break;
+                }
+            }
+            match next_child {
+                Some(ch) => {
+                    self.recursive_coords(ch, i, coords)
+                },
+                None => coords
+            }
+        }
+    }
+
+    pub fn coords(&self, i: usize) -> Vec<i32> {
+        self.recursive_coords(&self.tree, i, vec![])
+    }
 }
 
 fn redraw(siv: &mut Cursive) {
     // TODO: collect() feels excessive, but I can't beat the borrow checker without it
     let lines: Vec<String> = siv.with_user_data(|state: &mut State| state.lines().collect()).unwrap();
-    let mut select = siv.find_name::<SelectView>("select").unwrap();
-    select.clear();
-    select.add_all_str(lines);
+    siv.call_on_name("select", |select: &mut SelectView| {
+        let id = select.selected_id();
+        select.clear();
+        select.add_all_str(lines);
+        if let Some(id) = id {
+            select.set_selection(id);
+        }
+    });
+}
 }
 
 fn main() {
@@ -138,14 +188,24 @@ fn main() {
     siv.set_user_data(state);
 
     let xselect = OnEventView::new(select)
-        .on_event(Key::Right, move |s| { s.with_user_data(|state: &mut State| { state.collapsed.remove(&vec![0, 1, 1]); }); redraw(s); })
-        .on_event(Key::Left, move |s| { s.with_user_data(|state: &mut State| { state.collapsed.insert(vec![0, 1, 1]); }); redraw(s); });
+        .on_event(Key::Right, move |s| {
+            let id = s.call_on_name("select", |select: &mut SelectView| { select.selected_id().unwrap() }).unwrap();
+            s.with_user_data(|state: &mut State| {
+                let coords = state.coords(id + 1);
+                state.collapsed.remove(&coords);
+            });
+            redraw(s);
+        })
+        .on_event(Key::Left, move |s| {
+            let id = s.call_on_name("select", |select: &mut SelectView| { select.selected_id().unwrap() }).unwrap();
+            s.with_user_data(|state: &mut State| {
+                let coords = state.coords(id + 1);
+                state.collapsed.insert(coords);
+            });
+            redraw(s);
+        });
 
     siv.add_fullscreen_layer(xselect.scrollable().full_screen());
     siv.add_global_callback('q', Cursive::quit);
     siv.run();
-}
-
-#[cfg(test)]
-mod tests {
 }
